958b03488aab82f952c72cff2f9a23e84e2a371c
Daniwave100 <154252646+Daniwave100@users.noreply.github.com>
2026-01-20 16:29:37 -0500

    Changed to Alpaca backend

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -3,25 +3,19 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from services.market_data import MarketDataService
 from services.portfolio_service import PortfolioService
-from streamlit_searchbox import st_searchbox
+portfolio_service = PortfolioService("hi")
 
 st.title("📊 Portfolio Dashboard")
 
-portfolio = PortfolioService("temp")
-market_data = MarketDataService()
+positions = portfolio_service.sync_positions_with_alpaca()
 
-with st.sidebar:
+for position in positions:
+    df = portfolio_service.get_day_chart(position["ticker"])
 
-    st.header("Add positions to your portfolio")
-    selected_stock = st_searchbox(
-        search_function=market_data.search_for_dropdown,
-        placeholder="Search a ticker symbol",
-        key="stock_Search"
-    )
-    number_of_shares = st.number_input("Number of shares:", min_value=0.0, step=1.0)
-    purchase_price = st.number_input("Purchase Price:", min_value=0.0, step=1.0)
-
-    # splitting stock to get string before first " " space character
-    portfolio.add_position(selected_stock.split()[0], number_of_shares, purchase_price)
+    row = st.container(border=True, width="stretch", height="stretch")
+    col1, col2 = row.columns(2)
+    col1.markdown(f"##  {position['ticker']}")
+    col1.markdown(f"###### {position["shares"]}")
+    col2.line_chart(df, x="timestamp", y="close")
+    
