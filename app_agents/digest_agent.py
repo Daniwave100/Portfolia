49b3a369b269b3aeb61e410f50cb5f38bcb51872
Daniwave100 <154252646+Daniwave100@users.noreply.github.com>
2026-01-22 20:21:14 -0500

    Added initial digest agent functionality

--- app_agents/digest_agent.py
+++ app_agents/digest_agent.py
@@ -10,54 +10,52 @@ import json
 
 class DigestAgent:
     def __init__(self):
-        self.prompt = """
+        self.portfolio_service = PortfolioService("test")
+        self.tickers = [position["ticker"] for position in self.portfolio_service.sync_positions_with_alpaca()]
+        self.prompt = f"""
         You are a stock portfolio news summarizer.
         You will have access to a person's portfolio information, including stock positions, number of stocks, and more.
         You will write up 1-2 paragraph general important world news as it relates to the market.
         You will then include 2-4 paragraphs on important news related to each stock.
         You will return the sources you used for your output.
+
+        The stocks in the portfolio you will evaluate are {self.tickers}
         """
-        self.portfolio_service = PortfolioService("test")
-
-    def get_positions(self):
-        positions = self.portfolio_service.sync_positions_with_alpaca()
-        tickers = ""
-        for position in positions:
-            tickers += position["ticker"] + ","
-
-        tickers = tickers[:-1]
-        return tickers
 
     # tool to search web
-    def search_web_for_news(self, tickers):
-        # just following marketaux documentation for this
-        conn = http.client.HTTPSConnection("api.marketaux.com")
-        params = urllib.parse.urlencode({
-            "api_token": MARKET_AUX_KEY,
-            "symbols": tickers,
-            "limit": 3
-        })
-        conn.request("GET", "/v1/news/all?{}".format(params))
-        res = conn.getresponse()
-        data = res.read()
-        return data.decode("utf-8")
+    # @function_tool
+    def search_web_for_news(self):
+        all_news = ""
+        i = 0
+        for ticker in self.tickers:
+            # just following marketaux documentation for this
+            conn = http.client.HTTPSConnection("api.marketaux.com")
+            params = urllib.parse.urlencode({
+                "api_token": MARKET_AUX_KEY,
+                "symbols": ticker,
+                "limit": 3
+            })
+            conn.request("GET", "/v1/news/all?{}".format(params))
+            res = conn.getresponse()
+            data = res.read().decode("utf-8")
+            all_news += f"Article {i}:\n {data}" 
+            i += 1
+        return all_news
 
     # run agent
     def run_agent(self):
-        tickers = self.get_positions()
-        digest_agent = Agent(name="Digest Agent", instructions=self.prompt, tools=[self.search_web_for_news(tickers)])
-        tickers = self.get_positions()
-        result = Runner.run_sync(digest_agent, tickers)
+        all_news = self.search_web_for_news()
+        digest_agent = Agent(name="Digest Agent", instructions=self.prompt)
+        result = Runner.run_sync(digest_agent, all_news)
         print(result)
         return result
     
 if __name__ == "__main__":
-    
     agent = DigestAgent()
+    print(agent.tickers)
 
-    data = agent.search_web_for_news("ASTS,FCX")
-    test = json.dumps(data, indent=4)
-    print(test)
+    print(agent.run_agent())
+    
 
 # okay so far we are getting 3 news articles for the stocks we have in our portfolio
 # i have to look into i tmore because it seems like we're only getting news on the first stock
