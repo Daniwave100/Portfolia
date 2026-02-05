ab4bc451530ca14bf9e1479cfdd3e6231e9d5c92
Daniwave100 <154252646+Daniwave100@users.noreply.github.com>
2026-02-04 19:07:44 -0500

    working on lambda aws functionality

--- services/digest_service.py
+++ services/digest_service.py
@@ -7,13 +7,13 @@ import json
 from datetime import datetime
 from zoneinfo import ZoneInfo
 import boto3
-from config.settings import AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY
 
 class DigestService():
     def __init__(self):
         self.bucket_name = "portfolia-daily-digest"
         self.prefix = "digests"
         self.region = "us-east-2"
+        self.s3 = boto3.resource("s3", region_name=self.region) # Lambda / IAM role will automatically provide credentials
         
     def run_digest(self):
         agent = DigestAgent()
@@ -28,8 +28,7 @@ class DigestService():
 
         body = self.run_digest()
 
-        s3 = boto3.resource("s3")
-        s3.Object(self.bucket_name, s3_object_key).put(
+        self.s3.Object(self.bucket_name, s3_object_key).put(
             Body=body,
             ContentType="application/json; charset=utf-8"
         )
@@ -40,28 +39,30 @@ class DigestService():
         date_key = datetime.now(et).date().isoformat()
         s3_object_key = f"{self.prefix}/{date_key}.json"
 
-        s3 = boto3.client("s3", region_name=self.region)
-        s3 = boto3.client(
-            "s3",
-            aws_access_key_id=AWS_ACCESS_KEY,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            region_name=self.region,
-            )
+        s3_client = boto3.client("s3", region_name=self.region)
 
         try:
-            response = s3.get_object(Bucket=self.bucket_name, Key=s3_object_key)
+            response = s3_client.get_object(Bucket=self.bucket_name, Key=s3_object_key)
             body_bytes = response["Body"].read()
             return json.loads(body_bytes.decode("utf-8"))
-        except s3.exceptions.NoSuchKey:
+        except s3_client.exceptions.NoSuchKey:
             raise FileNotFoundError(f"No digest found at s3://{self.bucket_name}/{s3_object_key}")
     
+# not inside class
+def lambda_handler(event, context):
+    service = DigestService()
+    s3_path = service.digest_to_s3()
+
+    return {
+        "statusCode": 200,
+        "body": {
+            "message": "Digest generated successfully",
+            "s3_path": s3_path
+        }
+    }
 
 if __name__ == "__main__":
     digest_service = DigestService()
     path = digest_service.digest_to_s3()
     print(path)
 
-# run agent
-# save agent results to S3
-# streamlit fetches from S3
-# so forth
